a9611c3d3b23a5a19e2d5156b3b42c328bc10e97
Larry Gamboa <techmedia.lg@gmail.com>
2020-11-03 13:55:07 -0800

    pseudo code js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Set up variables
+var apiKey = "fb7c1ad92bb81728d12349821ce85842";
+var currentWeather = $("#currentWeather");
+var fiveDayForecast = $("#fiveDayForecast");
+
+
+// Set up local storage for city search
+
+
+// Create function to retrieve current weather
+
+
+// Create function to retrieve 5 day forecast
+
+
